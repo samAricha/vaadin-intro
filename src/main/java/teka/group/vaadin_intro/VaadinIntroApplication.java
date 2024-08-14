90055371faf68ebc601aa76db964e1280a8e03da
samAricha <samaricha001@gmail.com>
2024-08-14 22:34:46 +0300

    Initial commit

---
+++ src/main/java/teka/group/vaadin_intro/VaadinIntroApplication.java
@@ -0,0 +1,13 @@
+package teka.group.vaadin_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teka/group/vaadin_intro/VaadinIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package teka.group.vaadin_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
